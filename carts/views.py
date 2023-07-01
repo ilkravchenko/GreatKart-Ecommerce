fd340ba732b6dd15cabcbae96cdae0ba32f705b4
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-07-01 20:38:08 +0300

    Make Search functionality

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem
 from store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -63,6 +64,9 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, items=None):
     try:
+        tax = 0
+        grand_total = 0
+        
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -72,7 +76,7 @@ def cart(request, total=0, quantity=0, items=None):
         tax = (2 * total)/100
         grand_total = total + tax
         
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass
     
     context = {
