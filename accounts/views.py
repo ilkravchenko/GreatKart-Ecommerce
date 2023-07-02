2bbe244fba6df598ee2fc64f8354f1a4d9124dcf
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-07-02 20:31:03 +0300

    Create Login and Registration functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
+            
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email,
+                                               password=password, username=username)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registration Was Successfull')
+            
+            return redirect('register')
+    else:
+    
+        form = RegistrationForm()
+        
+    context = {
+        'form': form,
+    }
+    
+    return render(request, 'accounts/register.html', context=context)
+
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        user = auth.authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            # messages.success(request, 'You are now logged in')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentails')
+            return redirect('login')
+    
+    return render(request, 'accounts/login.html')
+
+
+@login_required(login_url = 'login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You are Loged out')
+    
+    return redirect('login')
