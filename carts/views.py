f91369f488d6a6ad5420966a614fc1c476a1d169
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-07-02 12:16:54 +0300

    Create Variation Model for Products and make it functional

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem
-from store.models import Product
+from store.models import Product, Variation
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
@@ -15,6 +15,20 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id) # get the product
+    product_variation = []
+    
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+            
+    
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
